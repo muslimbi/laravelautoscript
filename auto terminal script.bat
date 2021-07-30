5a352fe07660fdc7790e93db07d398d8a18051c2
Deen Islam <muslimbi@users.noreply.github.com>
2021-07-30 21:47:27 +0600

    Update auto terminal script.bat
    semi final modify

--- auto terminal script.bat
+++ auto terminal script.bat
@@ -1,37 +1,74 @@
 @echo off
-
 set projectname=hasan6
-composer create-project --prefer-dist laravel/laravel %projectname%
+
+cd /D %~dp0
+SET CURRENTDIR=%~dp0
+SET PROJECTDIR=%CURRENTDIR%%projectname%\
+
+
+call composer create-project --prefer-dist laravel/laravel %projectname%
+
+echo Laravel Install Sucessfully
 copy database.sqlite %projectname%\database\database.sqlite
+mkdir %projectname%\resources\views\layouts
+copy app.blade.php %projectname%\resources\views\layouts\app.blade.php
+
+echo Database Copy Sucessfully
+
 
 cd %projectname%
 
-echo DB_CONNECTION=sqlite
-echo DB_DATABASE=D:\laravel\%projectname%\database\database.sqlite
-
-composer require laravel/ui
-php artisan ui bootstrap
-php artisan ui bootstrap --auth
+echo %PROJECTDIR%
+echo %CURRENTDIR%
 
 
-composer require crestapps/laravel-code-generator --dev
 
-// app/Providers/AppServiceProvider.php register()
+@echo off &setlocal
+set "search=localhost"
+set "replace=localhost:8000"
+set textfile=.env
+set newfile=Output.txt
+(for /f "delims=" %%i in (%textfile%) do (
+    set "line=%%i"
+    setlocal enabledelayedexpansion
+    set "line=!line:%search%=%replace%!"
+    echo(!line!
+    endlocal
+))>"%newfile%"
+del %textfile%
+rename %newfile%  %textfile%
 
-//if ($this->app->runningInConsole()) {
-//    $this->app->register('CrestApps\CodeGenerator\CodeGeneratorServiceProvider');
-//}
+rem echo.APP_URL=http://localhost:8000>>"%PROJECTDIR%.env"
+echo.DB_CONNECTION=sqlite>>"%PROJECTDIR%.env"
+echo.DB_DATABASE=D:\laravel\%projectname%\database\database.sqlite>>"%PROJECTDIR%.env"
+
+echo ENV file modify sucessfully
+
+call composer require laravel/ui
+call php artisan ui bootstrap --auth
+echo Laravel UI Install Sucessfully
+
+call composer require laravel/telescope
+call php artisan telescope:install
+echo Laravel Telescope Install Sucessfully
 
 
-php artisan resource-file:create Biography --fields=name,age,biography,sport,gender,colors,is_retired,photo,range,month --translation-for=en,ar
-php artisan create:resources Biography --with-form-request --with-soft-delete --models-per-page=15 --with-migration
-php artisan create:resources Biography --with-form-request --with-soft-delete --models-per-page=15 --with-migration --fields=name,age,biography,sport,gender,colors,is_retired,photo,range,month --translation-for=en,ar
+call composer require crestapps/laravel-code-generator --dev
 
-php artisan resource-file:create AssetCategory --fields=name,age,biography,sport,gender,colors,is_retired,photo,range,month --translation-for=en,ar
-php artisan create:resources AssetCategory --with-form-request --with-soft-delete --models-per-page=15 --with-migration
-php artisan create:resources AssetCategory --with-form-request --with-soft-delete --models-per-page=15 --with-migration --fields=name,age,biography,sport,gender,colors,is_retired,photo,range,month --translation-for=en,ar
+echo Code Generator Install Sucessfully
+
+
+php artisan resource-file:create Biography --fields="id,name,name:gender;options:male|female;html-type:select;data-type:enum,name:music_type;html-type:checkbox;options:country|pop|rock|jazz|rap,is_retired"
+php artisan create:resources Biography --with-soft-delete --models-per-page=15 --with-migration
+
+php artisan resource-file:create Animal --fields=id,name,description,is_active --translation-for=en,ar
+php artisan create:resources Animal --with-soft-delete --models-per-page=15 --with-migration
+
+php artisan resource-file:create AssetCategory --fields=id,name,description,is_active
+php artisan create:resources AssetCategory --with-migration
 
 php artisan migrate
 php artisan serve
 
 
+pause
